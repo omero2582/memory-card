07d6157688704fa64f41ce340d11a154facd37b9
omero2582 <iiomero2582ii@gmail.com>
2023-08-28 23:24:02 -0400

    Implemented Settings Modal & adjusted dark theme
    Settings Modal holds a lot of the previous options in one html dialog
    element.
    Also added 2 icons.
    
    TODO TODO refer to prev commits
    But rn need to work on stylings for each card + card image, how I want
    scoreboard to look, and some cleanup from the rest of the current
    styles which I was still playing around with, so some css rules are
    repeated.
    Also add animation to modal probably
    + handle game over somehow. Prob want a simple modal
    + in the long long future, add cardback selection

--- src/components/GameOptions/GameOptions.js
+++ src/components/GameOptions/GameOptions.js
@@ -1,11 +1,27 @@
 import './GameOptions.css'
+import Icon from '@mdi/react';
+import { mdiCog } from '@mdi/js';
+import { ThemeContext } from '../../context/ThemeContext';
+import { useContext } from 'react';
+import { mdiSkipNext } from '@mdi/js';
 
-export default function GameOptions ({ handleNextLevel, handleShowAdvanced, showAdvanced}) {
+
+
+export default function GameOptions ({ openModal, handleNextLevel, handleShowAdvanced, showAdvanced}) {
+  const {theme} = useContext(ThemeContext);
+  
   return (
-    <section className='GameOptions'>
+    <section className={`GameOptions ${theme}`}>
       <h2 className='visually-hidden'>Game Options</h2>
-      <button onClick={handleNextLevel}>Next Level</button>
-      <button onClick={handleShowAdvanced}>{showAdvanced ? 'Hide' : 'Show'} Advanced</button>
+      <button onClick={openModal}>
+        <Icon path={mdiCog} size={1.2} />
+        Settings
+      </button>
+      {/* <button onClick={handleShowAdvanced}>{showAdvanced ? 'Hide' : 'Show'} Advanced</button> */}
+      <button onClick={handleNextLevel}>
+        <Icon path={mdiSkipNext} size={1.2} />
+        Next Level
+      </button>
     </section>
   )
 }
--- src/components/Scoreboard/Scoreboard.js
+++ src/components/Scoreboard/Scoreboard.js
@@ -5,13 +5,13 @@ export default function Scoreboard ({level, numCards, score, bestScore}) {
   return (
     <section className="Scoreboard">
       <h2 className="visually-hidden">Scoreboard</h2>
-      <div>
-        <p className="h2">Level: {level}</p>
-        <p className="h2">Cards: {numCards}</p>
-      </div>
       <div className="scores">
         <p className="h2">Score: {score}</p>
         <p className="h2">Best Score: {bestScore}</p>
+      </div>
+      <div>
+        <p className="h2">Level: {level}</p>
+        <p className="h2">Cards: {numCards}</p>
       </div>
     </section>
   )
